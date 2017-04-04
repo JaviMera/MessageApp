b48dcec2de894c5ad07d739dbd3eb27e3e3ab324
javie <javier_mera88@hotmail.com>
2017-04-04 11:06:13 -0500

    Added a progress bar to indicate the user that the application is in the process of creating the user. Refactored some of the code in signup activity as well.

--- app/src/main/java/com/teamtreehouse/ribbit/ui/SignUpActivity.java
+++ app/src/main/java/com/teamtreehouse/ribbit/ui/SignUpActivity.java
@@ -1,21 +1,22 @@
 package com.teamtreehouse.ribbit.ui;
 
-import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.DialogFragment;
+import android.support.v4.widget.ContentLoadingProgressBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.FirebaseDatabase;
 import com.teamtreehouse.ribbit.R;
 import com.teamtreehouse.ribbit.database.MessageDB;
 import com.teamtreehouse.ribbit.dialogs.DialogFragmentError;
@@ -27,16 +28,22 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
-public class SignUpActivity extends AppCompatActivity {
+public class SignUpActivity extends AppCompatActivity implements InsertCallback{
+
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+
+    @BindView(R.id.progressBar)
+    ContentLoadingProgressBar progressBar;
 
     @BindView(R.id.usernameField)
-    protected EditText mUsername;
+    protected EditText usernameView;
 
     @BindView(R.id.passwordField)
-    protected EditText mPassword;
+    protected EditText passwordView;
 
     @BindView(R.id.emailField)
-    protected EditText mEmail;
+    protected EditText emailView;
 
     @BindView(R.id.signupButton)
     protected Button mSignUpButton;
@@ -51,33 +58,39 @@ public class SignUpActivity extends AppCompatActivity {
         setContentView(R.layout.activity_sign_up);
 
         ButterKnife.bind(this);
+
+        setSupportActionBar(toolbar);
+        progressBar.setVisibility(View.INVISIBLE);
     }
 
+    private boolean isTextViewEmpty(TextView view) {
+
+        return view
+            .getText()
+            .toString()
+            .trim()
+            .isEmpty();
+    }
 
     @OnClick(R.id.signupButton)
     public void onSignUpClick(View view) {
 
-        String username = mUsername.getText().toString();
-        String password = mPassword.getText().toString();
-        String email = mEmail.getText().toString();
+        progressBar.setVisibility(View.VISIBLE);
 
-        username = username.trim();
-        password = password.trim();
-        email = email.trim();
-
-        if (username.isEmpty() || password.isEmpty() || email.isEmpty()) {
+        if (isTextViewEmpty(usernameView) || isTextViewEmpty(passwordView) || isTextViewEmpty(emailView)) {
 
             DialogFragment dialog = DialogFragmentError.newInstance(
                     getString(R.string.signup_error_message),
                     getString(R.string.signup_error_title));
 
             dialog.show(getSupportFragmentManager(), "dialog_signup");
-
-        } else {
+        }
+        else {
 
             FirebaseAuth auth = FirebaseAuth.getInstance();
-            final String finalEmail = email;
-            final String finalUsername = username;
+            final String email = emailView.getText().toString();
+            final String username = usernameView.getText().toString();
+            final String password = passwordView.getText().toString();
 
             auth
                 .createUserWithEmailAndPassword(email, password)
@@ -97,28 +110,11 @@ public class SignUpActivity extends AppCompatActivity {
                             FirebaseUser firebaseUser = task.getResult().getUser();
                             User newUser = new UserRequest(
                                 firebaseUser.getUid(),
-                                finalEmail,
-                                finalUsername
+                                email,
+                                username
                             );
 
-                            MessageDB.insertUser(newUser, new InsertCallback() {
-                                @Override
-                                public void onSuccess(User user) {
-
-                                    startActivity(new Intent(SignUpActivity.this, LoginActivity.class));
-                                    finish();
-                                }
-
-                                @Override
-                                public void onFailure(String message) {
-
-                                    DialogFragment dialog = DialogFragmentError.newInstance(
-                                            getString(R.string.existing_user_error_title),
-                                            message);
-
-                                    dialog.show(getSupportFragmentManager(), "dialog_signup");
-                                }
-                            });
+                            MessageDB.insertUser(newUser, SignUpActivity.this);
                         }
                     }
                 });
@@ -129,5 +125,24 @@ public class SignUpActivity extends AppCompatActivity {
     public void onCancelClick(View view) {
 
         finish();
+    }
+
+    @Override
+    public void onSuccess(User user) {
+
+        progressBar.setVisibility(View.INVISIBLE);
+        startActivity(new Intent(SignUpActivity.this, LoginActivity.class));
+        finish();
+    }
+
+    @Override
+    public void onFailure(String message) {
+
+        progressBar.setVisibility(View.INVISIBLE);
+        DialogFragment dialog = DialogFragmentError.newInstance(
+                getString(R.string.existing_user_error_title),
+                message);
+
+        dialog.show(getSupportFragmentManager(), "dialog_signup");
     }
 }
